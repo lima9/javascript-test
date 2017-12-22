20e679da8b78863a9235c8233ff7984ec738eb54
Max <max@jortiles.com>
2017-12-22 09:55:17 +0100

    odd ocurrencies done

--- Lesson 2/OddOccurrencesInArray.js
+++ Lesson 2/OddOccurrencesInArray.js
@@ -1,56 +1,45 @@
-var arr = [1,2,2,1,3,4,5,6,7,5,6,7,3];
-//The alone value is 4 in arr[5]
+var arr = [1, 2, 2, 1, 3, 4, 5, 6, 7, 5, 6, 7, 3];
 
-
-
-function sortArray(array) {
-  var done = false;
-  while (!done) {
-    done = true;
-    for (var i = 1; i < array.length; i += 1) {
-      if (array[i - 1] > array[i]) {
-        done = false;
-        var tmp = array[i - 1];
-        array[i - 1] = array[i];
-        array[i] = tmp;
+function bubbeSort(arr) {
+  var d = false;
+  while (!d) {
+    d = true;
+    for (var i = 0; i < arr.length; i++) {
+      if (arr[i - 1] > arr[i]) {
+        d = false
+        var tmp = arr[i - 1];
+        arr[i - 1] = arr[i];
+        arr[i] = tmp;
       }
     }
   }
-
- 
-  
-  document.writeln("ii " + array);
-    
-   return array;
+  return arr;
 }
 
 function solution(num) {
-    
-    //document.writeln("re  " + num)
-	var length = arr.length;
-    var alone = 0;
-    
-    if (length % 2 === 0){
-    	alert("Not odd length");
+  var length = arr.length;
+  if (length % 2 === 0) {
+    alert("Not odd length");
+  }
+  //We know that it is already sorted and of odd lenght
+  var found = 0;
+  var foundy = true;
+  while (!foundy)
+    foundy = true;
+  for (var j = 0; j < num.length; j++) {
+    if (num[j] == num[j + 1]) {
+      j++;
+      foundy = false;
+    } else if (num[j] != num[j + 1]) {
+      found = num[j];
+      foundy = true;
     }
-    
-    /*for (var i = 0; i < length; i ++){
-        for (var j = 1; j < length; j++){
-            if (num[i] === num[j]){
-               document.writeln(num[i] + "+"+ num[j] + "___");
-            }
-            //document.write("   Para i: " + 1 + "Tenemos las siguientes j: " + j);
-
-         }
-     }*/
-    //alert(num[5]);
-    //alert(alone);
-    
-    return length;
+  }
+  return found;
 }
 
-//document.writeln("We have the following Array: " + arr);
-var value = solution(arr);
-var or = sortArray(arr);
-//document.writeln(or);
-//document.writeln("The value that has no pair is: " + value);
+document.writeln("We have the following Array: " + arr);
+var or = bubbeSort(arr);
+var value = solution(or);
+document.writeln("The sorted array looks like this: " + or);
+document.writeln("The value that has no pair is: " + value);
